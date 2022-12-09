055d59b69930bb4566feb816f07748de0c78a24b
umer-redfire <sardar.umar@redfireai.com>
2022-12-09 11:58:28 +0500

    added cloudinary and file upload for user profile picture

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require("dotenv").config()
 const connectDB = require("./config/Db")
 const cookieParser  = require('cookie-parser')
 const bodyparser = require('body-parser')
+const cloudinary = require("cloudinary");
 const cors = require('cors')
 const questionRoute = require('./routes/questionRoute')
 const survayRoute = require('./routes/survayRoute')
@@ -10,7 +11,7 @@ const userRoute = require('./routes/userRoute')
 const auth = require('./routes/authRoute')
 const session = require('express-session');
 const cookieSession = require("cookie-session");
-
+const fileUpload = require("express-fileupload");
 const passport = require('passport');
 const errorHandler = require('./middleWare/errorMiddleware')
 
@@ -19,7 +20,8 @@ const app = express()
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(cookieParser());
-app.use(bodyparser.json())
+app.use(bodyparser.json());
+app.use(fileUpload());
 app.use(cors({origin: "http://localhost:3000",
 methods: "GET,POST,PUT,DELETE",
 credentials: true,}))
@@ -39,7 +41,12 @@ app.use(session({
 // initialize passport
 app.use(passport.initialize());
 app.use(passport.session());
-
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
+  
 
 app.use('/api/question', questionRoute)
 app.use('/api/survay', survayRoute)
@@ -51,4 +58,14 @@ app.use(errorHandler);
 
 connectDB()
 const port = process.env.PORT
-app.listen(port, () => console.log(`Server running......`))
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Server is working on http://localhost:${process.env.PORT}`);
+  });
+  process.on("unhandledRejection", (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+  
+    server.close(() => {
+      process.exit(1);
+    });
+  });
